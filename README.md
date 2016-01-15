f1431d80dc8aca1a60e68feb1aa93d1dade090c0
drewrwilson <drew.r.wilson@gmail.com>
2016-01-15 12:37:18 -0500

    pull count of signatures from formkeep api
    closes #4

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # signature-data-backend
-backend application that accepts high-volume form submissions and saves them to a database to be batch exported to a permanent data store
+
+
+current status:
+pulls data from FormKeep API for the one active petition
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var restify = require('restify');  //package for API routes
+var request = require("request");  //package to do http requests for data from form keep api
+
+
+var formKeepAPIKey = process.env.FORMKEEPAPIKEY; //set as an environmental variable
 
 //run the server
 var port = process.env.PORT || 80; //it's required to have this environmental variable set in on deploy on heroku
+
 
 var server = restify.createServer({
   name: 'signature-data-backend',
@@ -17,12 +22,28 @@ server.use(restify.bodyParser());
 
 // this is just a route for the index of the API
 server.get('/', function (req, res, next) {
-  var hardcoded = {
-    "totalCount": "4003"
+  var theOutput = {
+    "totalCount": "4003" //hardcoded default
   };
 
-  res.send(hardcoded);
-  return next();
+  // get totalCount from FormKeep API
+  var apiURL = 'https://formkeep.com/api/v1/forms/02515915d774/submissions.json?api_token=' + formKeepAPIKey;
+
+  request({
+      url: apiURL,
+      json: true
+  }, function (error, response, body) {
+
+      if (!error && response.statusCode === 200) {
+        theOutput.totalCount = body.meta.pagination.total_count;
+        console.log('totalCount: ');
+          console.log(theOutput.totalCount) // Print the json response
+          res.send(theOutput);
+          return next();
+      }
+  })
+
+
 });
 
 
